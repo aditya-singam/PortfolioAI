8af9598660a92afc6bf53d5659cbe664652ff471
Aditya Singampalli <adityasingampalli@Adityas-MacBook-Pro.local>
2024-01-06 23:46:53 -0800

    more chsnges

--- pages/3_Hobbies.py
+++ pages/3_Hobbies.py
@@ -10,7 +10,7 @@ local_css("style/style.css")
 
 st.sidebar.markdown(info['Photo'],unsafe_allow_html=True)
 
-img_1 = Image.open("images/1.jpg")
+img_1 = Image.open("images/1.png")
 img_2 = Image.open("images/2.png")
 img_3 = Image.open("images/3.png")
 
