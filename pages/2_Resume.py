a8c90dba3e2bc1a6a3c6994f4c97b752c1f8b94f
Aditya Singampalli <adityasingampalli@Adityas-MacBook-Pro.local>
2024-01-06 23:03:08 -0800

    more chsnges

--- pages/2_Resume.py
+++ pages/2_Resume.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import base64
 from constant import *
+from PIL import Image
+from constant import *
 
 def local_css(file_name):
     with open(file_name) as f:
@@ -14,8 +16,13 @@ st.title("📝 Resume")
 
 st.write("[Click here if it's blocked by your browser](https://cognitiveclass.ai/)")
 
-with open("images/resume.pdf","rb") as f:
-      base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-      pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
-      st.markdown(pdf_display, unsafe_allow_html=True)
-  
+img_3 = Image.open("images/resume.png")
+st.image(img_3)
+
+##
+##with open("images/resume.png","rb") as f:
+##      base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+##      pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
+##      st.markdown(pdf_display, unsafe_allow_html=True)
+##  
+##
